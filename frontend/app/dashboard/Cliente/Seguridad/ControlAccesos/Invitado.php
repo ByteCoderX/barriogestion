0a2e3883b8b0b5a22b1b926782995b35925fa4e9
juli20251 <juliplay708@gmail.com>
2025-09-30 23:47:39 -0300

    corregi algunos colores

--- frontend/app/dashboard/Cliente/Seguridad/ControlAccesos/Invitado.php
+++ frontend/app/dashboard/Cliente/Seguridad/ControlAccesos/Invitado.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Usuario - Registro de Invitados</title>
-    <link rel="stylesheet" href="invitado.css?=18">
+    <link rel="stylesheet" href="invitado.css">
     <link rel="stylesheet" href="../../index.css?v=46">
 
 </head>
