f1390b5b5c4e405b337c2e2f0f51ded09e04062d
juli20251 <juliplay708@gmail.com>
2025-10-06 11:56:19 -0300

    seccion notificaciones lista para mostrar en presentacion

--- frontend/app/config/config.php
+++ frontend/app/config/config.php
@@ -1,13 +1,13 @@
-<?php
+<?php 
+//CONNECT TO DATABASE 
+$host = 'localhost'; 
+$db = 'tesisde'; 
+$user = 'tesisde'; 
+$pass = 'quepasalarva'; 
 
-//CONNECT TO DATABASE
-$host = 'localhost';
-$db = 'tesisde';
-$user = 'tesisde';
-$pass = 'quepasalarva';
+$loginURL = "https://panchosrv.bringfeel.com.ar/tesisde/app/login.html"; 
+http://localhost/barriogestion/frontend/app/login.html
+$registerURL = "https://panchosrv.bringfeel.com.ar/tesisde/app/register.html"; 
 
-$loginURL = "https://panchosrv.bringfeel.com.ar/tesisde/app/login.html";
-$registerURL = "https://panchosrv.bringfeel.com.ar/tesisde/app/register.html";
-$dashboardURL = "https://panchosrv.bringfeel.com.ar/tesisde/app/index.php";
-
+$dashboardURL = "https://panchosrv.bringfeel.com.ar/tesisde/app/index.php"; 
 ?>
