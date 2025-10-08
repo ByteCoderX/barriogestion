16de105d96d513a430f6a9108ebb00b16996995e
BringFeel <vizgarra362@gmail.com>
2025-10-08 17:54:13 -0300

    fix(diContainer): Se agregaron 2 dependencias al contenedor de cliente.

--- backend/src/diContainer/clientContainer.ts
+++ backend/src/diContainer/clientContainer.ts
@@ -3,18 +3,31 @@ import { DiContainer } from './DiContainer'
 
 // Prisma Repositories
 import { PrismaExpensesRepository } from '@app/client/expenses/repositories/PrismaExpensesRepository'
+import { PrismaReservationsRepository } from '@app/client/reservations/repositories/PrismaReservationsRepository'
 
 // Services
 import { ExpensesServices } from '@app/client/expenses/ExpensesServices'
+import { ReservationsServices } from '@app/client/reservations/ReservationsServices'
 
 const clientContainer = new DiContainer()
 
 // Expenses Modules
 const expensesRepository = new PrismaExpensesRepository()
 
+// Reservations Modules
+const reservationsRepository = new PrismaReservationsRepository()
+
 clientContainer.registerInstance('expenses-repository', expensesRepository)
 clientContainer.register('expenses-services', ExpensesServices, [
   'expenses-repository',
+])
+
+clientContainer.registerInstance(
+  'reservations-repository',
+  reservationsRepository,
+)
+clientContainer.register('reservations-services', ReservationsServices, [
+  'reservations-repository',
 ])
 
 export { clientContainer }
