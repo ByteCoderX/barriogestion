ee834efc8b129f4340148e72ff615af9d1477488
Francisco Santiago Vizgarra <69062690+BringFeel@users.noreply.github.com>
2025-10-12 20:58:30 -0300

    fix(cors): Se agregaron varias configuraciones a cors.

--- backend/src/shared/middlewares/applyAppMiddlewares.ts
+++ backend/src/shared/middlewares/applyAppMiddlewares.ts
@@ -3,9 +3,16 @@ import cors from 'cors'
 import { loggerMiddleware } from './loggerMiddleware'
 import cookieParser from 'cookie-parser'
 
+const corsOptions = {
+  origin: 'https://panchosrv.bringfeel.com.ar',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
+}
+
 export const applyAppMiddlewares = (app: express.Application) => {
   app.disable('x-powered-by')
-  app.use(cors())
+  app.use(cors(corsOptions))
   app.use(loggerMiddleware)
   app.use(express.json())
   app.use(cookieParser())
