be29ea19d8ae4c0dc6eb48159acdf3d204687a6a
juli20251 <juliplay708@gmail.com>
2025-10-20 15:44:49 -0300

    feature(GestionLotes): se agrego la nueva seccion para el control de lotes para los administradores

--- frontend/app/Utils/auth/validator.php
+++ frontend/app/Utils/auth/validator.php
@@ -1,39 +1,30 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-if (!isset($_COOKIE['refreshToken'])) {
-    header("Location: ../../login.html");
-    exit;
-}
-
-function cargarSettings() {
-    $rootPath = dirname(__DIR__, 2);
-    $json = file_get_contents($rootPath . '/settings.json');
-    return json_decode($json, true);
-}
-
-$settings = cargarSettings();
-$apiUrl = $settings['API_URL'];
-
-$refreshToken=$_COOKIE['refreshToken'];
-$endpoint = $apiUrl . '/bg/v1/auth/validator';
-
-$ch=curl_init($endpoint);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    "Content-Type: application/json",
-    "x-api-key: hola",
-    "Cookie: refreshToken=$refreshToken"
-]);
-$response=curl_exec($ch);
-$httpCode=curl_getinfo($ch, CURLINFO_HTTP_CODE);
-curl_close($ch);
-
-if ($httpCode !== 200) {
-    header("Location: ../../login.html");
-    exit;
-}
-?>
+les column."
+  },
+  "popupTipLocalRules": {
+    "message": "Reglas locais: esta columna é para as reglas que afectan só a un sitio. As reglas locais anteponse ás globais.",
+    "description": "Tooltip when hovering the top-most cell of the local-rules column."
+  },
+  "popupTipSaveRules": {
+    "message": "Fai click para gardar os cambios.",
+    "description": "Tooltip when hovering over the padlock in the dynamic filtering pane."
+  },
+  "popupTipRevertRules": {
+    "message": "Fai click para desfacer os cambios.",
+    "description": "Tooltip when hovering over the eraser in the dynamic filtering pane."
+  },
+  "popupAnyRulePrompt": {
+    "message": "todo",
+    "description": ""
+  },
+  "popupImageRulePrompt": {
+    "message": "imaxes",
+    "description": ""
+  },
+  "popup3pAnyRulePrompt": {
+    "message": "de terceiros",
+    "description": ""
+  },
+  "popup3pPassiveRulePrompt": {
+    "message": "Imaxes/css de terceiros",
+    "description": ""
+  
